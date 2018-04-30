bfe6657c60753b6c478f787e67ae396e04c64bd6
AlinaDenisova <denicov_vasya@mail.ru>
2018-04-30 16:50:16 +0400

    Исправляет замечания

--- source/js/script.js
+++ source/js/script.js
@@ -1,8 +1,6 @@
 var header = document.querySelector(".page-header");
 var toggle = document.querySelector(".page-header__toggle");
-
   header.classList.remove("page-header--nojs");
-
   toggle.addEventListener("click", function(evt) {
     evt.preventDefault();
     if (header.classList.contains("page-header--closed")) {
@@ -14,11 +12,10 @@ var toggle = document.querySelector(".page-header__toggle");
     }
   });
 
-var link = document.querySelector("good-hit__btn");
-var catalogLink = document.querySelector("catalog-card__basket");
+var link = document.querySelector(".good-hit__btn");
+var catalogLink = document.querySelector(".catalog-card__basket");
 var modal = document.querySelector(".modal");
 var overlay = document.querySelector(".overlay");
-
   if (link) {
     link.addEventListener ("click", function (evt) {
       evt.preventDefault();
@@ -26,7 +23,6 @@ var overlay = document.querySelector(".overlay");
       overlay.classList.add("overlay--show");
     });
   }
-
   if (catalogLink) {
     catalogLink.addEventListener ("click", function (evt) {
       evt.preventDefault();
@@ -34,7 +30,6 @@ var overlay = document.querySelector(".overlay");
       overlay.classList.add("overlay--show");
     });
   }
-
   if (overlay) {
     overlay.addEventListener("click", function(event) {
       event.preventDefault();
@@ -42,7 +37,6 @@ var overlay = document.querySelector(".overlay");
       overlay.classList.remove("overlay--show");
     });
   }
-
   window.addEventListener("keydown", function (evt) {
     if (evt.keyCode === 27) {
       evt.preventDefault();
@@ -52,3 +46,23 @@ var overlay = document.querySelector(".overlay");
       }
     }
   });
+
+  function initMap() {
+  var centerLatLng = new google.maps.LatLng(59.938827, 30.323073);
+  var mapOptions = {
+        center: centerLatLng,
+        zoom: 16
+  };
+  var map = new google.maps.Map(document.querySelector(".contacts__map"), mapOptions);
+  var addressLatLng = new google.maps.LatLng(59.938827, 30.323073);
+
+  var isIE11 = !!(navigator.userAgent.match(/Trident/) && navigator.userAgent.match(/rv[ :]11/));
+  var marker = new google.maps.Marker({
+    position: addressLatLng,
+    map: map,
+    title: "ул. Большая Конюшенная 19/8, Санкт-Петербург",
+    icon: isIE11 ? "img/icon-map-pin.png" : "img/icon-map-pin.svg"
+  });
+}
+
+google.maps.event.addDomListener(window, "load", initMap);
